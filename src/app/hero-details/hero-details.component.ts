4b1a006936e2a4fcf64ef3735ce4c04522fe81d8
ram2509 <ramchaurasiya2509@gmail.com>
2018-01-02 15:24:55 -0500

    bugs remove

--- src/app/hero-details/hero-details.component.ts
+++ src/app/hero-details/hero-details.component.ts
@@ -16,11 +16,12 @@ export class HeroDetailsComponent implements OnInit {
               private location: Location) { }
 
   ngOnInit() {
+    this.getHero();
   }
   getHero(): void {
     const id = +this.activeRoute.snapshot.paramMap.get('id');
     this.heroService.getHero(id).subscribe(hero => this.hero = hero);
-    console.log(this.hero);
+     console.log(this.hero);
   }
   goBack() {
     this.location.back();
